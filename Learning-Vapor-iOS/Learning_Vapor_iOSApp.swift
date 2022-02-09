9cdabfcfe1f4996177358c4b459423735c6fe7e4
Matt Wylder <matt@wylder.us>
2022-02-09 02:33:55 -0600

    Initial Commit

---
+++ Learning-Vapor-iOS/Learning_Vapor_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_Vapor_iOSApp.swift
+//  Learning-Vapor-iOS
+//
+//  Created by Matthew Wylder on 2/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Learning_Vapor_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
